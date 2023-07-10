b93bf4d8a4e3298e97adaf55e74258274d1209e3
Todd Garrison <toddlgarrison@gmail.com>
2023-07-10 15:43:56 -0400

    First commit

---
+++ July 10/annotations/variables.ts
@@ -0,0 +1,37 @@
+let apples: number = 5;
+let speed: string = "fast";
+let hasName: boolean = false;
+
+let nothingMuch: null = null;
+
+//built in objects
+
+let now: Date = new Date();
+
+//Array
+
+let colors: string[] = ["black", "white", "blue"];
+let myNumber: number[] = [1,2,3,4,5]
+let truths: boolean[] = [true, false, false, true]
+
+//Classes
+
+class Car {
+
+}
+
+let car: Car = new Car();
+
+//Object literal
+
+let point: { x:number; y: number } = {
+    x: 10,
+    y: 20
+}
+
+//Function
+
+const logNumber: (i: number) => void = (i: number) => {
+    
+    console.log(i);
+};
