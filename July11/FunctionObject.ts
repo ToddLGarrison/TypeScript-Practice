bc44a3e4020108cb73d47a4eae37770d9fa81109
Todd Garrison <toddlgarrison@gmail.com>
2023-07-11 15:06:28 -0400

    named and unnamed functions in TS

--- July11/FunctionObject.ts
+++ July11/FunctionObject.ts
@@ -10,3 +10,10 @@ const subtract = (a: number, b: number) => {
 const subtract2 = (a: number, b: number): number => {
     return a - b;
 }
+function divide(a: number, b: number): number {
+    return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+    return a * b;
+}
