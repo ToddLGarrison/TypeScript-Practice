cab0b2f76547ad0bb2f22d5aec184eab954d5b0b
Todd Garrison <toddlgarrison@gmail.com>
2023-07-11 15:15:27 -0400

    destructure function

--- July11/FunctionObject.ts
+++ July11/FunctionObject.ts
@@ -17,3 +17,32 @@ function divide(a: number, b: number): number {
 const multiply = function (a: number, b: number): number {
     return a * b;
 }
+
+
+//void means no return from this function
+const logger =(message: string ): void => {
+    console.log(message);
+}
+
+//never means we will never reach end of function
+const throwError = (message: string): never =>{
+    throw new Error(message);
+}
+
+const forecastWeather = {
+    date: new Date(),
+    weather: 'humid'
+}
+
+const logWeather = (forecastWeather: {date: Date, weather: string }): void=>{
+    console.log(forecastWeather.date)
+    console.log(forecastWeather.weather)
+}
+
+//destructure
+const logWeather = ({date, weather}: {date: Date, weather: string }): void=>{
+    console.log(forecastWeather.date)
+    console.log(forecastWeather.weather)
+}
+
+logWeather(forecastWeather)
