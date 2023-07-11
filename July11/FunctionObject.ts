e61d167b2db829ed8a3ccc9c692bef667fb9f405
Todd Garrison <toddlgarrison@gmail.com>
2023-07-11 15:03:53 -0400

    Subtract Void function

--- July11/FunctionObject.ts
+++ July11/FunctionObject.ts
@@ -1,3 +1,12 @@
 const add = (a: number, b: number): number => {
     return a + b;
 }
+
+//returns VOID
+const subtract = (a: number, b: number) => {
+    a - b;
+}
+
+const subtract2 = (a: number, b: number): number => {
+    return a - b;
+}
