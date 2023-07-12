7787b2358acbe1b2ab167ad136c0e37e12f23556
Todd Garrison <toddlgarrison@gmail.com>
2023-07-12 16:33:35 -0400

    Interfaces part 1

---
+++ July12/interfaces.ts
@@ -0,0 +1,30 @@
+interface Vehicle {
+    name: string;
+    year: number;
+    broken: boolean;
+    summary(): string;
+}
+
+const oldCivic = {
+    name: 'civic',
+    year: 2000,
+    broken: true,
+    summary(): string {
+        return `Name: ${this.name}`;
+    }
+};
+
+// const printVehicle = (vehicle: {name: string; year: number; broken: boolean}) : void => {
+//     console.log(`Name = ${vehicle.name}`)
+//     console.log(`Year = ${vehicle.year}`)
+//     console.log(`Broken? = ${vehicle.broken}`)
+// };
+
+const printVehicle = (vehicle: Vehicle) : void => {
+    console.log(`Name = ${vehicle.name}`)
+    console.log(`Year = ${vehicle.year}`)
+    console.log(`Broken? = ${vehicle.broken}`)
+};
+
+
+printVehicle(oldCivic);
