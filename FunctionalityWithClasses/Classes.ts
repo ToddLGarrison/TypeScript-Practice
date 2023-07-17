928e644a9f7ba54b034d3671481dffe0a3fa06a9
Todd Garrison <toddlgarrison@gmail.com>
2023-07-17 15:17:22 -0400

    Method Modifiers

--- FunctionalityWithClasses/Classes.ts
+++ FunctionalityWithClasses/Classes.ts
@@ -1,10 +1,84 @@
+// class Vehicle {
+//     drive(): void {
+//         console.log('drive me!');
+//     }
+
+//     honk(): void {
+//         console.log('beep beep!');
+//     }
+// }
+
+
+// //Inheritance
+
+// class Car extends Vehicle {
+//     drive():void {
+//         console.log('vrrroooom!');
+//     }
+// }
+
+
+
+// const carr = new Car()
+
+// carr.drive()
+// carr.honk()
+
+
+// class Vehicle {
+//     // public drive(): void {
+//     //     console.log('chuggga chugga!');
+//     // }
+
+//     public honk(): void {
+//         console.log('beep beep!');
+//     }
+// }
+
+// class Car extends Vehicle {
+//     private drive(): void {
+//         console.log("vrroooom!");
+//     }
+
+//     startDrivingProcess(): void {
+//         this.drive();
+//     }
+// }
+
+// const carr = new Car()
+
+// carr.startDrivingProcess()
+// carr.honk()
+
 class Vehicle {
-    drive(): void {
-        console.log('drive me!');
+    // public drive(): void {
+    //     console.log('chuggga chugga!');
+    // }
+
+    // public honk(): void {
+    //     console.log('beep beep!');
+    // }
+
+    protected honk(): void {
+        console.log('beep beep!');
     }
 }
 
-const vehicle = new Vehicle();
+const vehicle = new Vehicle()
+vehicle.honk()
 
-vehicle.drive();
+class Car extends Vehicle {
+    private drive(): void {
+        console.log("vrroooom!");
+    }
 
+    startDrivingProcess(): void {
+        this.drive();
+        this.honk()
+    }
+}
+
+const carr = new Car()
+
+carr.startDrivingProcess()
+carr.honk()
